3238361d6aa511fd9d1d8b0cc94401cb8e640469
Rupinder <rupindermonga@gmail.com>
2020-05-04 19:20:28 +0530

    upto totalRating

--- testing2.py
+++ testing2.py
@@ -27,30 +27,7 @@ for eachFile in json_files:
 d = {"Name": json_files, "loaded_flag":loaded_flag}
 data_loaded_flag = pd.DataFrame(d)
 
-
-# def readingJsonFiles(path_to_json):
-#     json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
-#     not_loaded_files = []
-#     loaded_files = []
-#     loaded_full_files = []
-#     loaded_flag = []
-#     for eachFile in json_files:
-#         try:
-#             with open(os.path.join(path_to_json, eachFile)) as f:
-#                 new_file = json.load(f)
-#                 loaded_full_files.append(new_file)
-#                 loaded_files.append(eachFile)
-#                 loaded_flag.append("Green")
-#         except ValueError:
-#             not_loaded_files.append(eachFile)
-#             loaded_flag.append("Red")
-#     d = {"Name": json_files, "loaded_flag":loaded_flag}
-#     data_loaded_flag = pd.DataFrame(d)
-#     return json_files, not_loaded_files, loaded_full_files, loaded_files, data_loaded_flag
-
 def countingReviews():
-#    loaded_files = readingJsonFiles(path_to_json)[3]
- #   loaded_full_files = readingJsonFiles(path_to_json)[2]
     review_flag = []    
     for eachFile in loaded_full_files:
         if len(eachFile['reviews']) == 0:
@@ -62,8 +39,6 @@ def countingReviews():
     return data_review_flag
 
 def countingTitle():
-    #loaded_files = readingJsonFiles(path_to_json)[3]
-    #loaded_full_files = readingJsonFiles(path_to_json)[2]
     title_flag = []    
     for eachFile in loaded_full_files:
         if len(eachFile['title']) == 0:
@@ -74,18 +49,52 @@ def countingTitle():
     data_review_flag = pd.DataFrame(d)
     return data_review_flag
 
+def countingFeatures():
+    features_flag = []    
+    for eachFile in loaded_full_files:
+        if len(eachFile['features']) == 0:
+            features_flag.append("Yellow")
+        else:
+            features_flag.append("Green")
+    d = {"Name":loaded_files, "features_flag": features_flag}
+    data_features_flag = pd.DataFrame(d)
+    return data_features_flag
 
+def countingRating():
+    rating_flag = []    
+    for eachFile in loaded_full_files:
+        if len(eachFile['rating']) == 0:
+            rating_flag.append("Yellow")
+        else:
+            rating_flag.append("Green")
+    d = {"Name":loaded_files, "rating_flag": rating_flag}
+    data_rating_flag = pd.DataFrame(d)
+    return data_rating_flag
 
+def countingTotalRatings():
+    totalRatings_flag = []    
+    for eachFile in loaded_full_files:
+        if len(eachFile['totalRatings']) == 0:
+            totalRatings_flag.append("Yellow")
+        else:
+            totalRatings_flag.append("Green")
+    d = {"Name":loaded_files, "totalRatings_flag": totalRatings_flag}
+    data_totalRatings_flag = pd.DataFrame(d)
+    return data_totalRatings_flag
 
 
 def writingFile():
     start_time = time.time()
-#    data_loaded_flag =  readingJsonFiles(path_to_json)[4]
     data_review_flag = countingReviews()
     data_title_flag = countingTitle()
+    data_features_flag = countingFeatures()
+    data_rating_flag = countingRating()
+    data_totalRatings_flag = countingTotalRatings()
     file1 = pd.merge(data_loaded_flag, data_review_flag, on = "Name", how = "left")
-    final_flag = pd.merge(file1, data_title_flag, on = "Name", how = "left")
-
+    file2 = pd.merge(file1, data_title_flag, on = "Name", how = "left")
+    file3 = pd.merge(file2, data_features_flag, on = "Name", how = "left")
+    file4 = pd.merge(file3, data_rating_flag, on = "Name", how = "left")
+    final_flag = pd.merge(file4, data_totalRatings_flag, on = "Name", how = "left")
     final_flag.to_csv("file_list.csv")
     return time.time() - start_time
 

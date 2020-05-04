5960edfa6f752bb292244f40d583e35a12c839f0
Rupinder <rupindermonga@gmail.com>
2020-05-04 19:07:35 +0530

    upto title - products, increase speed

--- testing2.py
+++ testing2.py
@@ -2,35 +2,55 @@ import os, json
 import dateparser
 import csv
 import pandas as pd
-
+import time
 
 #path_to_json = input("Enter the folder address containing json files: ")
 #json_files = [pos_json for pos_json in os.listdir(path_to_json)]
 path_to_json = '/media/rupinder/C49A5A1B9A5A0A76/Users/Rupinder/Desktop/BVR/Data/laptop/raw/'
 
-def readingJsonFiles(path_to_json):
-    json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
-    not_loaded_files = []
-    loaded_files = []
-    loaded_full_files = []
-    loaded_flag = []
-    for eachFile in json_files:
-        try:
-            with open(os.path.join(path_to_json, eachFile)) as f:
-                new_file = json.load(f)
-                loaded_full_files.append(new_file)
-                loaded_files.append(eachFile)
-                loaded_flag.append("Green")
-        except ValueError:
-            not_loaded_files.append(eachFile)
-            loaded_flag.append("Red")
-    d = {"Name": json_files, "loaded_flag":loaded_flag}
-    data_loaded_flag = pd.DataFrame(d)
-    return json_files, not_loaded_files, loaded_full_files, loaded_files, data_loaded_flag
+
+json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+not_loaded_files = []
+loaded_files = []
+loaded_full_files = []
+loaded_flag = []
+for eachFile in json_files:
+    try:
+        with open(os.path.join(path_to_json, eachFile)) as f:
+            new_file = json.load(f)
+            loaded_full_files.append(new_file)
+            loaded_files.append(eachFile)
+            loaded_flag.append("Green")
+    except ValueError:
+        not_loaded_files.append(eachFile)
+        loaded_flag.append("Red")
+d = {"Name": json_files, "loaded_flag":loaded_flag}
+data_loaded_flag = pd.DataFrame(d)
+
+
+# def readingJsonFiles(path_to_json):
+#     json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+#     not_loaded_files = []
+#     loaded_files = []
+#     loaded_full_files = []
+#     loaded_flag = []
+#     for eachFile in json_files:
+#         try:
+#             with open(os.path.join(path_to_json, eachFile)) as f:
+#                 new_file = json.load(f)
+#                 loaded_full_files.append(new_file)
+#                 loaded_files.append(eachFile)
+#                 loaded_flag.append("Green")
+#         except ValueError:
+#             not_loaded_files.append(eachFile)
+#             loaded_flag.append("Red")
+#     d = {"Name": json_files, "loaded_flag":loaded_flag}
+#     data_loaded_flag = pd.DataFrame(d)
+#     return json_files, not_loaded_files, loaded_full_files, loaded_files, data_loaded_flag
 
 def countingReviews():
-    loaded_files = readingJsonFiles(path_to_json)[3]
-    loaded_full_files = readingJsonFiles(path_to_json)[2]
+#    loaded_files = readingJsonFiles(path_to_json)[3]
+ #   loaded_full_files = readingJsonFiles(path_to_json)[2]
     review_flag = []    
     for eachFile in loaded_full_files:
         if len(eachFile['reviews']) == 0:
@@ -42,8 +62,8 @@ def countingReviews():
     return data_review_flag
 
 def countingTitle():
-    loaded_files = readingJsonFiles(path_to_json)[3]
-    loaded_full_files = readingJsonFiles(path_to_json)[2]
+    #loaded_files = readingJsonFiles(path_to_json)[3]
+    #loaded_full_files = readingJsonFiles(path_to_json)[2]
     title_flag = []    
     for eachFile in loaded_full_files:
         if len(eachFile['title']) == 0:
@@ -59,14 +79,15 @@ def countingTitle():
 
 
 def writingFile():
-    data_loaded_flag =  readingJsonFiles(path_to_json)[4]
+    start_time = time.time()
+#    data_loaded_flag =  readingJsonFiles(path_to_json)[4]
     data_review_flag = countingReviews()
     data_title_flag = countingTitle()
     file1 = pd.merge(data_loaded_flag, data_review_flag, on = "Name", how = "left")
     final_flag = pd.merge(file1, data_title_flag, on = "Name", how = "left")
 
     final_flag.to_csv("file_list.csv")
-    return "File is printed"
+    return time.time() - start_time
 
 
 f = writingFile()
